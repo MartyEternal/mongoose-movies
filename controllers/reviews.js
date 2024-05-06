fbc6a72c17e28e122d0ca61ad65f997e4fb69aab
MartyEternal <imartyl@hotmail.com>
2024-05-06 20:02:47 +1000

    new repo

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,11 +1,19 @@
 const Movie = require('../models/movie');
 
 module.exports = {
-  create
+  create,
+  delete: deleteReview,
+  edit,
+
 };
 
 async function create(req, res) {
   const movie = await Movie.findById(req.params.id);
+
+  req.body.user = req.user._id;
+  req.body.userName = req.user.name;
+  req.body.userAvatar = req.user.avatar;
+
   // We can push (or unshift) subdocs into Mongoose arrays
   movie.reviews.push(req.body);
   try {
@@ -17,3 +25,33 @@ async function create(req, res) {
   // Step 5:  Respond to the Request (redirect if data has been changed)
   res.redirect(`/movies/${movie._id}`);
 }
+
+async function deleteReview(req, res) {
+  const movie = await Movie.findOne({ "reviews._id": req.params.id, "reviews.user": req.user._id });
+  if (!movie) return res.redirect("/movies");
+  movie.reviews.remove(req.params.id);
+  await movie.save();
+  res.redirect(`/movies/${movie._id}`);
+};
+
+async function edit(req, res) {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    if (!movie) {
+      return res.status(404).send("Movie not found");
+    };
+    const review = movie.reviews.find(function (review) {
+      return review._id == reviewId;
+    });
+
+    if (!review) {
+      return res.status(404).send("Review not found");
+    };
+    res.render(`/movies/${movie._id}/edit`, {
+      title: "Edit Review"
+    });
+  } catch (err) {
+    console.log(err);
+  };
+};
+
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const reviewsCtrl = require('../controllers/reviews');
+const ensureLoggedIn = require("../config/ensureLoggedIn");
 
+router.put("/reviews/:id/edit", ensureLoggedIn, reviewsCtrl.edit);
 // POST /movies/:id/reviews (create review for a movie)
-router.post('/movies/:id/reviews', reviewsCtrl.create);
+router.post('/movies/:id/reviews', ensureLoggedIn, reviewsCtrl.create);
+router.delete("/reviews/:id", ensureLoggedIn, reviewsCtrl.delete);
 
 module.exports = router;
